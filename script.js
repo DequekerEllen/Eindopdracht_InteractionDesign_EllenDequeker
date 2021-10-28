bfc7ec547dd04b7627466a9b95d5b3b3b5a7911c
DequekerEllen <ellen.dequeker@student.howest.be>
2021-10-28 15:47:04 +0200

    rain animation

--- script.js
+++ script.js
@@ -2,92 +2,41 @@ let url, response, locationUrl;
 
 let locationNr = 31810;
 
-// const weather = {
-//   LocalObservationDateTime: '2021-10-27T12:13:00+02:00',
-//   EpochTime: 1635329580,
-//   WeatherText: 'Mostly cloudy',
-//   WeatherIcon: 6,
-//   HasPrecipitation: false,
-//   PrecipitationType: null,
-//   IsDayTime: true,
-//   Temperature: {
-//     Metric: {
-//       Value: 13.5,
-//       Unit: 'C',
-//       UnitType: 17,
-//     },
-//     Imperial: {
-//       Value: 56.0,
-//       Unit: 'F',
-//       UnitType: 18,
-//     },
-//   },
-//   MobileLink: 'http://www.accuweather.com/en/be/kortrijk/31810/current-weather/31810?lang=en-us',
-//   Link: 'http://www.accuweather.com/en/be/kortrijk/31810/current-weather/31810?lang=en-us',
-// };
+var makeItRain = function () {
+  //clear out everything
+  $('.rain').empty();
 
-// const Kortrijk = {
-//   Version: 1,
-//   Key: '31810',
-//   Type: 'City',
-//   Rank: 43,
-//   LocalizedName: 'Kortrijk',
-//   EnglishName: 'Kortrijk',
-//   PrimaryPostalCode: '',
-//   Region: {
-//     ID: 'EUR',
-//     LocalizedName: 'Europe',
-//     EnglishName: 'Europe',
-//   },
-//   Country: {
-//     ID: 'BE',
-//     LocalizedName: 'Belgium',
-//     EnglishName: 'Belgium',
-//   },
-//   AdministrativeArea: {
-//     ID: 'VWV',
-//     LocalizedName: 'West Flanders',
-//     EnglishName: 'West Flanders',
-//     Level: 1,
-//     LocalizedType: 'Province',
-//     EnglishType: 'Province',
-//     CountryID: 'BE',
-//   },
-//   TimeZone: {
-//     Code: 'CEST',
-//     Name: 'Europe/Brussels',
-//     GmtOffset: 2.0,
-//     IsDaylightSaving: true,
-//     NextOffsetChange: '2021-10-31T01:00:00Z',
-//   },
-//   GeoPosition: {
-//     Latitude: 50.826,
-//     Longitude: 3.264,
-//     Elevation: {
-//       Metric: {
-//         Value: 13.0,
-//         Unit: 'm',
-//         UnitType: 5,
-//       },
-//       Imperial: {
-//         Value: 42.0,
-//         Unit: 'ft',
-//         UnitType: 0,
-//       },
-//     },
-//   },
-//   IsAlias: false,
-//   SupplementalAdminAreas: [],
-//   DataSets: ['AirQualityCurrentConditions', 'AirQualityForecasts', 'Alerts', 'ForecastConfidence', 'FutureRadar', 'MinuteCast', 'Radar'],
-// };
+  var increment = 0;
+  var drops = '';
+  var backDrops = '';
+
+  while (increment < 100) {
+    //couple random numbers to use for various randomizations
+    //random number between 98 and 1
+    var randoHundo = Math.floor(Math.random() * (98 - 1 + 1) + 1);
+    //random number between 5 and 2
+    var randoFiver = Math.floor(Math.random() * (5 - 2 + 1) + 2);
+    //increment
+    increment += randoFiver;
+    //add in a new raindrop with various randomizations to certain CSS properties
+    drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+  }
+
+  $('.rain.front-row').append(drops);
+  document.body.style.background = 'linear-gradient(to bottom, #202020, #111119)';
+};
 
 let showResultWeather = (queryResponse, queryL) => {
-  console.log(queryResponse);
+  console.log(queryResponse[0]);
   console.log(queryL);
   console.log(queryResponse[0].WeatherText);
   const temp = document.querySelector('.temp');
   let temperatuur = queryResponse[0].Temperature.Metric.Value;
-  temp.innerHTML = `${temperatuur}`;
+  temp.innerHTML = `${temperatuur} °C`;
+
+  // if (queryResponse[0].WeatherText == 'Mostly cloudy') {
+  //   makeItRain();
+  // }
 
   console.log(queryL.EnglishName);
   console.log(' ');
@@ -120,44 +69,28 @@ let getAPIWeather = async (location) => {
   showResultWeather(result, resultL);
 };
 
-// let getAPILocation = async (location) => {
-//   let key = '0lmagZR3gu9O2bu19fD1nE0w7laTeRHZ';
-//   locationUrl = `http://dataservice.accuweather.com/locations/v1/${location}?apikey=${key}`;
-//   console.log(locationUrl);
-
-//   const request = await fetch(`${locationUrl}`);
-//   const result = await request.json();
-
-//   showResultLocation(result);
-// };
-
 function click_btn() {
   const btn = document.querySelector('.js_next');
-  console.log(btn);
   btn.addEventListener('click', function () {
     console.log('geklikt');
-    locationNr += 1;
+    locationNr += 5;
     console.log(locationNr);
-    // getAPILocation(locationNr);
     getAPIWeather(locationNr);
     return locationNr;
   });
 
   const knop = document.querySelector('.js_previous');
-  console.log(knop);
   knop.addEventListener('click', function () {
     console.log('geklikt');
-    locationNr -= 1;
+    locationNr -= 5;
     console.log(locationNr);
     getAPIWeather(locationNr);
-    // getAPILocation(locationNr);
     return locationNr;
   });
 }
 
 function setup() {
   getAPIWeather(locationNr);
-  // getAPILocation(locationNr);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
